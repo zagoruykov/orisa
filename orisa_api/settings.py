9110ad620a1f5c59d8dbac8733166caacb365831
vzagoruyko <vitalik.zgrk@yandex.ru>
2023-03-21 10:27:34 +0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.github/workflows/build-and-deploy.yml

--- orisa_api/settings.py
+++ orisa_api/settings.py
@@ -22,7 +22,7 @@ AUTH_USER_MODEL = "staff.CustomUser"
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
