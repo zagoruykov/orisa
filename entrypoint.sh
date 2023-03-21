bf163ae6edf4fa33399c2573231c74fdde896997
vzagoruyko <vitalik.zgrk@yandex.ru>
2023-03-21 14:12:16 +0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.github/workflows/build-and-deploy.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
-poetry run python manage.py flush --no-input
 poetry run python manage.py migrate
 poetry run python manage.py collectstatic --no-input --clear
 poetry run gunicorn orisa_api.wsgi:application --bind 0.0.0.0:8000
